7b519f54db02e4f11fe0d2ee26b7838ffa36ea9d
David <davidroussenq@gmail.com>
2021-09-19 18:43:45 -0300

    aulas salvas
    atualizando

---
+++ Curso JavaScript/aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 50.5
+
+if (vel > 60) {
+    console.log(`Sua velocidade é ${vel}km/h`)
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+} else {
+    console.log(`Parabéns! Você esta dirigindo dentro do limite de velocidade!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
+
+
